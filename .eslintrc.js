d709d42850438c3dbe0c8ad8f4b22c06a9a0d786
pajecawav <pajecawav@gmail.com>
2021-10-16 21:32:17 +0300

    chore: add linting scripts and update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "parser": "@typescript-eslint/parser",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"plugin:vue/essential",
+		"eslint:recommended",
+		"@vue/typescript/recommended",
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+		parser: "@typescript-eslint/parser",
+	},
+	plugins: ["vue", "@typescript-eslint"],
+	rules: {},
 };
--- src/components/MinigameLink.vue
+++ src/components/MinigameLink.vue
@@ -30,6 +30,7 @@
 </template>
 
 <script setup lang="ts">
+import { defineProps } from "vue";
 import { RouterLink } from "vue-router";
 
 defineProps({
